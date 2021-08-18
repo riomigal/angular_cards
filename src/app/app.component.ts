6a97f16e3611bb293eed71190a11ce464940d8d7
Saverio Migale <saveriomigale@MacBook-Pro.local>
2021-08-18 12:48:26 +0200

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cards';
+  posts = [
+    {
+      title: 'Tree',
+      username: 'tree',
+      content: 'Tree for everyone, nature and love',
+      imageUrl: 'assets/images/tree.jpeg',
+    },
+    {
+      title: 'Biking',
+      username: 'biking',
+      content: 'huhuuu bycicle time',
+      imageUrl: 'assets/images/biking.jpeg',
+    },
+    {
+      title: 'Mountain',
+      username: 'mountain',
+      content: 'huhuuu mountain hiking time',
+      imageUrl: 'assets/images/mountain.jpeg',
+    },
+  ];
 }
